c0a837c7a919657671a596717c05721f9d2e2487
CubeBuilder <CubeBuilder@web.de>
2024-11-10 19:11:35 +0100

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,11 @@
 setInterval(function() {
-  if localStorage.getItem('login') == 'true' {
+  const loginStatus = localStorage.getItem('login');
+  const currentPage = window.location.href;
+
+  if (currentPage.includes('login.html') && loginStatus === 'true') {
     window.location.href = 'https://cubeindustry.github.io/cloud/index.html';
-  } else{
+  }
+  else if (currentPage.includes('index.html') && loginStatus === 'false') {
     window.location.href = 'https://cubeindustry.github.io/cloud/login.html';
   }
 }, 2000);
